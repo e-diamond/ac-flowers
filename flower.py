b5000997d01d567e153601f2627f42753edcb63e
e-diamond <68610675+e-diamond@users.noreply.github.com>
2021-11-04 14:24:31 +0000

    Changing namespace of flower-related variables Also calculates a second generation of flowers

--- flower.py
+++ flower.py
@@ -3,13 +3,34 @@ from utility import *
 
 class Flower:
 
+    # list of all valid flower species
+    species_list = ("rose", "tulip", "pansy", "cosmos", "lily", "hyacinth", "windflower", "mum")
+
+    # read data from json files
     color_data = JSON.readFromFile("json/colors.json")
+    seed_data = JSON.readFromFile("json/seeds.json")
 
     def __init__(self, genes, species):
         self.genes = genes
         self.species = species
         self.color = Flower.getColor(genes, species)
 
+    # flowers with the same attributes are equivalent
+    def __eq__(self, other):
+        if isinstance(other, Flower):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def isSeed(self):
+        return self.genes in Flower.seed_data[self.species]
+
     @staticmethod
     def getColor(genes, species):
         return Flower.color_data[decimal(genes, 3)][species]
+
+    # generate flowers available from seed bags
+    @staticmethod
+    def getSeeds(species):
+        seed_genes = Flower.seed_data[species]
+        return [Flower(genes, species) for genes in seed_genes]
--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 from flower import Flower
 from breeding_result import BreedingResult
-from json_handling import JSON
 
-flower_types = ("rose", "tulip", "pansy", "cosmos", "lily", "hyacinth", "windflower", "mum")
-
+# determine species of interest
 species = input("Flower species: ").lower()
 if species == "chrysanthemum":
     species = "mum"
 
-if species in flower_types:
-    # get seed genes for species
-    seed_data = JSON.readFromFile("json/seeds.json")
-    seed_genes = seed_data[species]
+# check if species is valid
+if species in Flower.species_list:
 
     # generate flowers produced by seeds
-    seeds = [Flower(genes, species) for genes in seed_genes]
+    seeds = Flower.getSeeds(species)
 
-    # generate breeding pairs
-    pairs = []
-    for i in range(len(seeds)):
-        for j in range(i, len(seeds)):
-            pairs.append(BreedingResult(seeds[i], seeds[j]))
+    produced = []
+    for i in range(2):
+        print("GENERATION", i, "\n")
 
-    for pair in pairs:
-        print("Parents: ", pair.f1.genes, pair.f1.color, pair.f2.genes, pair.f2.color)
-        for child in pair.children:
-            print(child[0].genes, child[0].color, child[1])
+        # generate breeding pairs
+        pairs = []
+        for i in range(len(seeds)):
+            for j in range(i, len(seeds)):
+                if seeds[j] in produced:
+                    continue
+                else:
+                    pairs.append(BreedingResult(seeds[i], seeds[j]))
+
+        new_gen = []
+        for pair in pairs:
+            print("Parents: ", pair.f1.genes, pair.f1.color, pair.f2.genes, pair.f2.color)
+            for child in pair.children:
+                print(child[0].genes, child[0].color, child[1])
+                new_gen.append(child[0])
+        print("\n\n")
+
+        produced = produced + seeds
+        seeds = seeds + new_gen
 
 
 else:
